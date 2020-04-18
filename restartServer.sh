ac16000c85b7c2924e00cfab8d9d990dcb189e91
sbgh <33097880+sbgh@users.noreply.github.com>
2020-04-18 07:42:23 -0600

    Added upgrade to restartServer, components

--- restartServer.sh
+++ restartServer.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
-cd /home/ec2-user/builder;
+
+#upgrade builder
+cd ~
+curl https://codeload.github.com/sbgh/builder/zip/master > builder.zip;
+unzip -oq builder.zip;
+yes | cp -fr ./builder-master/SystemsJSON.json ./builder/SystemsJSON.json;
+yes | cp -fr ./builder-master/BuildCode.json ./builder/BuildCode.json;
+yes | cp -fr ./builder-master/builder.js ./builder/builder.js;
+yes | cp -fr ./builder-master/views/* ./builder/views/;
+yes | cp -fr ./builder-master/uploads/* ./builder/uploads/;
+yes | cp -fr ./builder-master/static/* ./builder/static/;
+yes | cp -fr ./builder-master/setup/* ./builder/setup/;
+unzip -oq ./builder/uploads/uploads.zip -d ./builder/uploads;
+
+#launch builder
+cd ~/builder;
 pwd;
 echo;
 echo Stopping server builder.js..;
